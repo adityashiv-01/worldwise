d285259433c41b0e4874ad8719d61f9afb2ec55a
adityashiv-01 <adityashivagarwal@gmail.com>
2025-06-24 13:13:36 +0530

    static layout complete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>
+        &copy; {new Date().getFullYear} Worldwise Inc. All rights reserved
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
